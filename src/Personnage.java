59d9526ba13baf0bb721164a92366f8764d97e32
Tomtombinary <warhamerchaos@hotmail.fr>
2017-03-04 22:25:08 +0100

    Physique

--- src/Personnage.java
+++ src/Personnage.java
@@ -6,13 +6,29 @@ public class Personnage {
 	String nom;
 	int pv;
 	
-	int x;
-	int y;
+	double x;
+	double y;
+	
+	double vx;
+	double vy;
+	
+	double ax;
+	double ay;
+	
 	
 	public Personnage(String nom,int pv)
 	{
 		this.nom = nom;
 		this.pv = pv;
+		
+		this.x = 0;
+		this.y = 0;
+		
+		this.vy = -10.0;
+		this.vx = 2.0;
+		
+		this.ax = 0;
+		this.ay = 0.2;
 	}
 	
 	public void attaquer(Personnage cible)
@@ -22,12 +38,31 @@ public class Personnage {
 	
 	public void deplacer()
 	{
-		this.x+=1;
-		this.y+=1;
+		this.x+=vx;
+		this.y+=vy;
+		this.vx+=ax;
+		this.vy+=ay;
+		
+		if(this.y >= 400)
+		{
+			this.vy = -this.vy;
+		}
+		if(this.x >= 400)
+		{
+			this.vx = -this.vx;
+		}
+		if(this.x < 0)
+		{
+			this.vx = -this.vx;
+		}
+		if(this.y < 0)
+		{
+			this.vy = -this.vy;
+		}
 	}
 	
 	public void dessiner(Graphics g)
 	{
-		g.fillRect(x, y,10,10);
+		g.fillRect((int)x,(int)y,10,10);
 	}
 }
