1feeecbaced1b0e5e93fb90a12b1ed1a953dd4e5
Tomtombinary <warhamerchaos@hotmail.fr>
2017-03-04 21:33:42 +0100

    Personnage

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,11 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world !");
+		Personnage paul = new Personnage("Paul",100);
+		Personnage pierre = new Personnage("Pierre",100);
+		
+		paul.attaquer(pierre);
+		System.out.println(pierre.pv);
 	}
 
 }
